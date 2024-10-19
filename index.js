c3b4911f51e0bf485792dab88a625b050c3def9b
Lucas Hayashi <lucashayashi5@gmail.com>
2024-10-19 20:39:13 -0300

    Adicionado verificação de cors e handler de erro customizado

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ const person = require('./routes/person');
 const auth = require('./routes/auth');
 const user = require('./routes/user');
 
+const allowedHost = process.env.ALLOWED_HOST;
+const environment = process.env.ENVIRONMENT;
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (environment === 'production') {
+            if (origin === allowedHost) {
+                callback(null, true);
+            } else {
+                console.error(`CORS Error: Origin ${origin} is not allowed`);
+                callback(new Error('CORS Error: Access denied. This origin is not allowed.'), false);
+            }
+        } else {
+            callback(null, true);
+        }
+    }
+};
+
+function customErrorHandler(err, req, res, next) {
+    if (err.message.startsWith('CORS')) {
+        return res.status(403).json({ error: err.message });
+    }
+    
+    res.status(400).json({ error: 'An error occurred', details: err.message });
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cors());
 
@@ -24,6 +52,8 @@ app.use('/search', search)
 app.use('/movie', movie)
 app.use('/tv', tv)
 app.use('/person', person)
+
+app.use(customErrorHandler);
 
 app.listen(PORT, function (err) {
     if (err) console.log(err);
